75df72d5a764a00e6dc842d6acf0c66a057df010
sondhg <sondhw@gmail.com>
2025-12-28 14:51:52 +0700

    update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
-#include <Arduino.h> // PIO cần dòng này
-#include <WiFi.h>
+#include <Arduino.h>      // PIO cần dòng này
 #include <PubSubClient.h> // Thư viện MQTT
+#include <WiFi.h>
 
 // --- CẤU HÌNH CỦA BẠN ---
 const char *ssid = "sondhg_shared";            // Tên Wi-Fi Hotspot của bạn
@@ -12,8 +12,7 @@ WiFiClient espClient;
 PubSubClient client(espClient);
 
 // Hàm này chạy 1 lần khi ESP32 khởi động
-void setup()
-{
+void setup() {
   Serial.begin(115200); // Mở cổng Serial để theo dõi log
   Serial.println();
   Serial.print("Connecting to Wi-Fi: ");
@@ -23,8 +22,7 @@ void setup()
   WiFi.begin(ssid, password);
 
   // Chờ đến khi kết nối thành công
-  while (WiFi.status() != WL_CONNECTED)
-  {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
@@ -39,24 +37,19 @@ void setup()
 }
 
 // Hàm này kết nối lại MQTT nếu bị mất kết nối
-void reconnect()
-{
-  while (!client.connected())
-  {
+void reconnect() {
+  while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Tạo một Client ID ngẫu nhiên
     String clientId = "ESP32Client-";
     clientId += String(random(0xffff), HEX);
 
     // Thử kết nối
-    if (client.connect(clientId.c_str()))
-    {
+    if (client.connect(clientId.c_str())) {
       Serial.println("connected");
       // Gửi (Publish) 1 tin nhắn ngay khi kết nối
       client.publish("iot/project/test", "Hello from ESP32 (via PIO)");
-    }
-    else
-    {
+    } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
@@ -72,10 +65,8 @@ long lastMsg = 0;
 // --- Hết phần code mới ---
 
 // Hàm này lặp đi lặp lại mãi mãi
-void loop()
-{
-  if (!client.connected())
-  {
+void loop() {
+  if (!client.connected()) {
     reconnect(); // Nếu mất kết nối, kết nối lại
   }
   client.loop(); // Duy trì kết nối MQTT
@@ -85,8 +76,7 @@ void loop()
   long now = millis();
 
   // Cứ mỗi 5 giây (5000 mili giây) thì gửi 1 tin
-  if (now - lastMsg > 5000)
-  {
+  if (now - lastMsg > 5000) {
     lastMsg = now;
 
     // Tạo nội dung tin nhắn, ví dụ "Message so 1", "Message so 2"...
